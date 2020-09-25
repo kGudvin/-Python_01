bdca7b874f0056142469ae160dc0d1c0872566f2
kGudvin <kirillgudeta@gmail.com>
2020-09-25 05:29:14 +0300

    .

--- ex06.py
+++ ex06.py
@@ -1,6 +1,5 @@
 first_result = int(input('Введите результат первого дня: '))
 final_result = int(input('Введите конечный результат: '))
-
 counter = 1
 while first_result < final_result:
     first_result *= 1.1
